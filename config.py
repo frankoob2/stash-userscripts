9b9a84bcf847be8b2639c1b713c1ecb3c4b5b77b
frankoob2 <89805376+frankoob2@users.noreply.github.com>
2022-12-27 07:52:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 GITHUB_ROOT_URL = r"https://raw.githubusercontent.com/7dJx1qP/stash-userscripts/%%BRANCH%%/"
 BUNDLE_VERSION = "0.21.1"
-SERVER_URL = "http://localhost:9999"
+SERVER_URL = "http://100.125.79.102:6969"
 NAMESPACE = "https://github.com/7dJx1qP/stash-userscripts"
